c3efa41df7f2e27b6495a7ce1c47dd48fa22bfc7
Björn Daigger <124874016+bjoerndaigger@users.noreply.github.com>
2023-10-20 14:04:17 +0200

    contact form works

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -6,16 +6,33 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
   styleUrls: ['./contact-form.component.scss'],
 })
 export class ContactFormComponent {
-  @ViewChild('myForm') myForm!: ElementRef;
-  @ViewChild('nameField') nameField!: ElementRef;
-  @ViewChild('messageField') messageField!: ElementRef;
-  sendMail() {
-    // action="https://bjoerndaigger.de/send_mail/send_mail.php"
-    const nameValue = this.nameField.nativeElement.value;
-    const messageValue = this.messageField.nativeElement.value;
-  
-    console.log('Sending mail', nameValue, messageValue);
+  @ViewChild('myForm') myForm: ElementRef;
+  @ViewChild('nameField') nameField: ElementRef;
+  @ViewChild('messageField') messageField: ElementRef;
+  @ViewChild('sendButton') sendButton: ElementRef;
+
+  async sendMail() {
+    // Werte aus Textfeldern auslesen (im Prinzip das gleiche wie document.getElementById)
+    let nameField = this.nameField.nativeElement;
+    let messageField = this.messageField.nativeElement;
+    let sendButton = this.sendButton.nativeElement;
+    // Eingabefelder deaktivieren, anschließend könnte man z.B. eine "Senden"-Animation triggern
+    nameField.disabled = true;
+    messageField.disabled = true;
+    sendButton.disabled = true;
+    // Daten zum senden vorbereiten
+    let fd = new FormData();
+    fd.append('name', nameField.value);
+    fd.append('message', messageField.value);
+    // an URL senden
+    await fetch('https://bjoerndaigger.de/send_mail/send_mail.php', {
+      method: 'POST',
+      body: fd,
+    });
+    // Hier eventuell Nachricht gesendet Animation anzeigen und dann Eingabefelder wieder aktivieren
+    nameField.disabled = false;
+    messageField.disabled = false;
+    sendButton.disabled = false;
   }
 }
 
- 
